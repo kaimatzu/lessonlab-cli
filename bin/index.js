eed81a6370ff14e8adcfbb3107be337f6026229b
Karl John Villardar <74708425+kaimatzu@users.noreply.github.com>
2023-11-09 08:42:33 +0800

    Fixed CLI to support conditional compilation

--- bin/index.js
+++ bin/index.js
@@ -15,13 +15,18 @@ const options = yargs.usage("Usage:\n\
  -- test < Build flag > Development builds\n\ -- release < Build flag > Release builds ")
 .option("t", {
     alias: "test",
-    describe: "Build flag. Open's the application using dioxus-web. Intended for hot-reloading support for ease of development.",
+    describe: "Build flag. Compile the application using dioxus-web. Intended for hot-reloading support for ease of development.",
     type: "flag"
 }).option("r", {
     alias: "release",
-    describe: "Build flag. Open's the application using dioxus-desktop.",
+    describe: "Build flag. Compile the application using dioxus-desktop.",
     type: "flag"
-}).check((argv) => {
+}).option("b", {
+    alias: "build",
+    describe: "Build flag. Build the application.",
+    type: "flag"
+})
+.check((argv) => {
     // Check if both 'test' and 'release' options are provided
     if (argv.t == null && argv.r == null) {
         ll_logr.ll_error('Specify a build flag!');
@@ -37,7 +42,7 @@ const options = yargs.usage("Usage:\n\
 const dioxusCli = ['dioxus-cli', 'dx.exe'];
 
 // Check if dioxus-cli is installed
-    exec('cargo install --list', (error, stdout, stderr) => {
+exec('cargo install --list', (error, stdout, stderr) => {
     if (error) {
         ll_logr.ll_error(`Error executing the command: ${
             error.message
@@ -64,7 +69,6 @@ const dioxusCli = ['dioxus-cli', 'dx.exe'];
 
     }
     const cargoTarget = 'Cargo.toml';
-    const mainTarget = 'src/main.rs';
     const currentDirectory = process.cwd();
 
     // Check if the file exists in the current directory
@@ -79,106 +83,77 @@ const dioxusCli = ['dioxus-cli', 'dx.exe'];
         }
     });
 
-    const mainPath = path.join(currentDirectory, mainTarget);
-    fs.access(mainPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            ll_logr.ll_error(`LessonLab CLI Error: Failed to find directory containing ${mainTarget}.`);
-            process.exit(1); 
-        } else {
-            ll_logr.ll_info(`Found ${mainTarget}!`);
-            ll_logr.ll_trace(`Configuring ${mainTarget}...`);
-        }
-    });
-
     if (options.test) {
-        ll_logr.ll_info(`Running test build...`);
+        ll_logr.ll_info(`Compiling test build...`);
         
         // Configures the Cargo.toml file
         const cargoToml = fs.readFileSync(cargoTarget, 'utf8');
         const cargoLines = cargoToml.split('\n');
         for (let i = 0; i < cargoLines.length; i++) {
-            if (cargoLines[i].startsWith('# ') && cargoLines[i].includes('dioxus-web')) {
-                // Remove '# ' from the line
-                cargoLines[i] = cargoLines[i].substring(2);
-            }
-            if (!cargoLines[i].startsWith('# ') && cargoLines[i].includes('dioxus-desktop')) {
-                // Add '# ' to the line
-                cargoLines[i] = '# ' + cargoLines[i];
+            if (cargoLines[i].includes('default = ["desktop"]')) {
+                // Replace 'default = ["desktop"]' with 'default = ["web"]'
+                cargoLines[i] = cargoLines[i].replace('default = ["desktop"]', 'default = ["web"]');
             }
         }
         const modifiedCargoToml = cargoLines.join('\n');
 
-        // Configures the main.rs file
-        const mainRs = fs.readFileSync(mainTarget, 'utf8');
-        const regexPattern = /dioxus_desktop::launch/g;
-        const modifiedMainRs = mainRs.replace(regexPattern, 'dioxus_web::launch');
-
         fs.writeFileSync(cargoTarget, modifiedCargoToml, 'utf8');
-        fs.writeFileSync(mainTarget, modifiedMainRs, 'utf8');
 
-        const deploy = exec('dx serve --hot-reload', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error executing the command: ${error.message}`);
-                process.exit(1); 
-            }
-        
-            console.log(stdout);
-        });
-        
-        deploy.stdout.on('data', (data) => {
-            console.log(data);
-        });
-        
-        deploy.stderr.on('data', (data) => {
-            console.error(data);
-        });
-        
+        if (options.build) {
+            const deploy = exec('dx serve --hot-reload', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing the command: ${error.message}`);
+                    process.exit(1); 
+                }
+            
+                console.log(stdout);
+            });
+
+            deploy.stdout.on('data', (data) => {
+                console.log(data);
+            });
+            
+            deploy.stderr.on('data', (data) => {
+                console.error(data);
+            });
+        }     
     } 
+
     if (options.release) {
-        ll_logr.ll_info(`Running release build...`);
+        ll_logr.ll_info(`Compiling release build...`);
 
         const cargoToml = fs.readFileSync(cargoTarget, 'utf8');
-
-        const lines = cargoToml.split('\n');
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i].startsWith('# ') && lines[i].includes('dioxus-desktop')) {
-                // Remove '# ' from the line
-                lines[i] = lines[i].substring(2);
-            }
-            if (!lines[i].startsWith('# ') && lines[i].includes('dioxus-web')) {
-                // Add '# ' to the line
-                lines[i] = '# ' + lines[i];
+        const cargoLines = cargoToml.split('\n');
+        for (let i = 0; i < cargoLines.length; i++) {
+            if (cargoLines[i].includes('default = ["web"]')) {
+                // Replace 'default = ["web"]' with 'default = ["desktop"]'
+                cargoLines[i] = cargoLines[i].replace('default = ["web"]', 'default = ["desktop"]');
             }
         }
 
         // Join the modified lines back into a single string
-        const modifiedCargoToml = lines.join('\n');
-
-        // Configures the main.rs file
-        const mainRs = fs.readFileSync(mainTarget, 'utf8');
-        const regexPattern = /dioxus_web::launch/g;
-        const modifiedMainRs = mainRs.replace(regexPattern, 'dioxus_desktop::launch');
+        const modifiedCargoToml = cargoLines.join('\n');
 
         fs.writeFileSync(cargoTarget, modifiedCargoToml, 'utf8');
-        fs.writeFileSync(mainTarget, modifiedMainRs, 'utf8');
 
-        const deploy = exec('dx serve --hot-reload --platform desktop', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error executing the command: ${error.message}`);
-                process.exit(1); 
-            }
-        
-            console.log(stdout);
-        });
-        
-        deploy.stdout.on('data', (data) => {
-            console.log(data);
-        });
-        
-        deploy.stderr.on('data', (data) => {
-            console.error(data);
-        });
-        
+        if (options.build) {
+            const deploy = exec('dx serve --hot-reload --platform desktop', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing the command: ${error.message}`);
+                    process.exit(1); 
+                }
+            
+                console.log(stdout);
+            });
+            
+            deploy.stdout.on('data', (data) => {
+                console.log(data);
+            });
+            
+            deploy.stderr.on('data', (data) => {
+                console.error(data);
+            });
+        } 
     }
 });
 
